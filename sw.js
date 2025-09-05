6bc73d046058ce27fab72dc24eb593a6ed4279e9
Riccardo Ponte <pontuz12346@outlook.it>
2025-09-05 14:21:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Definiamo un nome per la cache
+// Definiamo un nome per la nostra cache
 const CACHE_NAME = 'condo-app-pwa-cache-v1';
 
 // Elenco dei file fondamentali da salvare per il funzionamento offline
@@ -34,5 +34,6 @@ self.addEventListener('fetch', event => {
   );
 
 });
+
 
 
